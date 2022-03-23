899d74c94ff4e0eada9487218581a778c11b0adb
Bennyz2030 <nicholasbenzinger@gmail.com>
2022-03-23 13:59:12 -0700

    Add functionality for subtract, multiply, and divide
    Co-authored-by: <mtekiesd210309@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// Business Logic
+// Business logic:
 
 function add(number1, number2) {
   return number1 + number2;
@@ -16,9 +16,44 @@ function divide(number1, number2) {
   return number1 / number2;
 }
 
-// UI Logic
+// Everything below this line is user interface logic:
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
-alert(result);
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const addition = add(number1, number2);
+    $("#addresult").text(addition);
+  });
+});
+
+$(document).ready(function() {
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#sub1").val());
+    const number2 = parseInt($("#sub2").val());
+    const sub = subtract(number1, number2);
+    $("#subtractresult").text(sub);
+  });
+});
+
+$(document).ready(function() {
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#multi1").val());
+    const number2 = parseInt($("#multi2").val());
+    const multi = multiply(number1, number2);
+    $("#multiplyresult").text(multi);
+  });
+});
+
+$(document).ready(function() {
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#div1").val());
+    const number2 = parseInt($("#div2").val());
+    const div = divide(number1, number2);
+    $("#divideresult").text(div);
+  });
+});
